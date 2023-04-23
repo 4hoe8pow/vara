defea6d42ef368e7e2d017f8a47155bb4c639e5a
kustham <ludoludo@outlook.jp>
2023-04-24 00:05:03 +0900

    wipwipwip

--- app/observer/components/footer.tsx
+++ app/observer/components/footer.tsx
@@ -3,14 +3,18 @@ import Button from '@mui/material/Button'
 import ArrowBack from '@mui/icons-material/ArrowBack'
 import SendIcon from '@mui/icons-material/Send'
 import Stack from '@mui/material/Stack'
+import { invoke } from '@tauri-apps/api/tauri'
 
 export default function Footer() {
+
+    const commit = () => {invoke('score_update')}
+
     return (
         <Stack direction="row" spacing={2}>
-            <Button variant="outlined" startIcon={<ArrowBack />}>
+            <Button variant="outlined" startIcon={<ArrowBack />} >
                 Undo
             </Button>
-            <Button variant="contained" endIcon={<SendIcon />}>
+            <Button variant="contained" endIcon={<SendIcon />} onClick={commit}>
                 Commit
             </Button>
         </Stack>
--- src-tauri/src/controller.rs
+++ src-tauri/src/controller.rs
@@ -6,21 +6,32 @@ use crate::{
     },
 };
 use anyhow::Result;
+use tauri::Runtime;
 
-/// 戦績の取得コマンド
+/// Commitボタン対応コマンド
 #[tauri::command]
-pub fn fetch_results_init() -> Result<Vec<ResultPayload>, String> {
-    // DIコンテナの構築
-    let container = DIContainer::new();
-    // Resultユースケースの生成
-    let usecase = container.provide_usecase::<dyn ResultUsecase>();
-    usecase.fetch_results()
+fn score_update<R: Runtime>(
+    app: tauri::AppHandle<R>,
+    window: tauri::Window<R>,
+) -> Result<(), String> {
+    
+    Ok(())
 }
 
-/// 試合予定の登録コマンド
-#[tauri::command]
-pub fn register_pre_game(payload: PreGamePayload) -> Result<String> {
-    let container = DIContainer::new();
-    let usecase = container.provide_usecase::<dyn PreGameUsecase>();
-    usecase.register_pre_game(payload)
-}
+///// 戦績の取得コマンド
+//#[tauri::command]
+//pub fn fetch_results_init() -> Result<Vec<ResultPayload>, String> {
+//    // DIコンテナの構築
+//    let container = DIContainer::new();
+//    // Resultユースケースの生成
+//    let usecase = container.provide_usecase::<dyn ResultUsecase>();
+//    usecase.fetch_results()
+//}
+//
+///// 試合予定の登録コマンド
+//#[tauri::command]
+//pub fn register_pre_game(payload: PreGamePayload) -> Result<String> {
+//    let container = DIContainer::new();
+//    let usecase = container.provide_usecase::<dyn PreGameUsecase>();
+//    usecase.register_pre_game(payload)
+//}
