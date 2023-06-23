0759d2d64ecc1d74b880de0dd7aaec49f1b2deb2
kustham <ludoludo@outlook.jp>
2023-06-24 00:16:20 +0900

    :sparkles: controller window

---
+++ app/controller/page.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next'
+
+const Controller: NextPage = () => {
+    return (
+        <>
+            <div>host team</div>
+            <input />
+            <div>away team</div>
+            <input />
+        </>
+    )
+}
+
+export default Controller
